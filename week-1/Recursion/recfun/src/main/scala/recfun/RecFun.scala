af4ec93c693a527bf2986d0b1b77fad63bd2f864
mariasintea <maria.sintea@gmail.com>
2021-08-26 10:04:34 +0300

    Improved Recursion Assignment Code

--- week-1/Recursion/recfun/src/main/scala/recfun/RecFun.scala
+++ week-1/Recursion/recfun/src/main/scala/recfun/RecFun.scala
@@ -15,25 +15,25 @@ object RecFun extends RecFunInterface:
    * Exercise 1 - Pascal's Triangle
    */
   def pascal(c: Int, r: Int): Int = {
-    @tailrec
-    def loop(column: Int, row: Int, prevLine: Array[Int], currentLine: Array[Int]): Int = {
-      // first compute the current element
-      if (column == 0 || column == row)
-        currentLine(column) = 1
-      else
-        currentLine(column) = prevLine(column) + prevLine(column - 1)
+    def nextRow(prevLine: List[Int]): List[Int] = {
+      val pairs = prevLine.zip(prevLine.drop(1).appended(0))
+      pairs.foldLeft(List[Int](1)){
+        case (xs, (x, y)) => xs.appended(x + y)}
+    }
 
+    @tailrec
+    def loop(column: Int, row: Int, prevLine: List[Int], currentLine: List[Int]): Int = {
       // then check if it is the right one, otherwise move to the next element
       if (column == c && row == r)
         currentLine(c)
       else
         if (column == row)
-          loop(0, row + 1, currentLine, new Array[Int](row + 2))
+          loop(0, row + 1, currentLine, nextRow(currentLine))
         else
           loop(column + 1, row, prevLine, currentLine)
     }
 
-    loop(0, 0, new Array[Int](1), new Array[Int](1))
+    loop(0, 0, List[Int](1), List[Int](1))
   }
 
   /**
@@ -57,19 +57,19 @@ object RecFun extends RecFunInterface:
    */
   def countChange(money: Int, coins: List[Int]): Int = {
     @tailrec
-    def loop(coinIndex: Int, sumIndex: Int, dp: Array[Int]): Int = {
+    def loop(coinIndex: Int, sumIndex: Int, dp: List[Int]): Int = {
       // dp[index] = number of ways to change "index" money
        if (coinIndex == coins.length)
          dp(money)
        else
          if (sumIndex == money + 1)
           loop(coinIndex + 1, 0, dp)
-         else {
+         else
            if (coins(coinIndex) <= sumIndex)
-              dp(sumIndex) += dp(sumIndex - coins(coinIndex))
-           loop(coinIndex, sumIndex + 1, dp)
-         }
+             loop(coinIndex, sumIndex + 1, dp.updated(sumIndex, dp(sumIndex) + dp(sumIndex - coins(coinIndex))))
+           else
+             loop(coinIndex, sumIndex + 1, dp)
     }
 
-    loop(0, 0, Array(1) ++ Array.fill[Int](money)(0))
+    loop(0, 0, List(1) ++ List.fill[Int](money)(0))
   }
